64365a10df23da82f1678d90c5835d4b8f4aed82
dongil <dongce@gmail.com>
2016-07-10 14:58:28 +0900

    folding package

--- 47_init-folding.el
+++ 47_init-folding.el
@@ -1,3 +1,14 @@
 ;; -*- coding: utf-8; -*-
+* folding package
+#+BEGIN_SRC emacs-lisp
 
 (use-package html-fold)
+(use-package origami-mode
+:commands (origami-mode)
+:config
+  (evil-leader/set-key-for-mode 'origami-mode
+    "o" origami-mode-map
+    )
+)
+#+END_SRC
+
