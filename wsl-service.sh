c5a962e2fb55e6fad0cc34f5ed0adc4bfab784ad
dongil <dongce@gmail.com>
2020-01-09 09:55:32 +0900

    WSL 용 systemctl 파일

--- wsl-service.sh
+++ wsl-service.sh
@@ -5,8 +5,9 @@ done ;
 
 
 tmux new-session -s powershell -d
-tmux send -t  powershell "/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe" ENTER
-tmux send -t  powershell "Remove-Module -Name PSReadline" ENTER
+tmux send -t  powershell "/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -NoExit -Command  Remove-Module -Name PSReadline" ENTER
+sleep 5
+tmux send -t  powershell "echo DONE" ENTER
 
 tmux new-session -s imapget -d
 tmux send -t  imapget "cd /mnt/t/misc/eaglemail" ENTER
