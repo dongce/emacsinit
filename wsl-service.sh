9e8e7f1c91ba269fe715db5ad67351cc01f44219
dongil <dongce@gmail.com>
2020-02-11 16:20:36 +0900

    이번달은 항상 인덱스 한다.

--- wsl-service.sh
+++ wsl-service.sh
@@ -5,10 +5,12 @@ done ;
 
 
 tmux new-session -s powershell -d
-tmux send -t  powershell "/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -NoExit -Command  Remove-Module -Name PSReadline" ENTER
 sleep 5
+tmux send -t  powershell "/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -NoExit -Command  Remove-Module -Name PSReadline" ENTER
 tmux send -t  powershell "echo DONE" ENTER
 
+
+/opt/local/bin/mu index -v --nocolor --maildir /mnt/f/PERSONAL/2020/$(date +%m)
 tmux new-session -s imapget -d
 tmux send -t  imapget "cd /mnt/t/misc/eaglemail" ENTER
-tmux send -t  imapget "/opt/python38/bin/python3 imapget.py" ENTER
+tmux send -t  imapget "/opt/anaconda3/bin/python3 imapget.py" ENTER
