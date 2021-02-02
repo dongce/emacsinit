f59c5b504d41cd41fde5793ff1b2e300b0a5d704
dongil <dongce@gmail.com>
2021-02-02 17:18:15 +0900

    use emboard and prescient search

---
+++ dumpinit/23_init-search.el
@@ -0,0 +1,47 @@
+;; -*-mode: emacs-lisp; coding: utf-8; buffer-read-only: t;-*-
+
+(leaf consult-selectrum
+  :ensure t
+  :bind (((kbd "<f9>") . selectrum-repeat)
+         ((kbd "<S-f9>") . hangul-to-hanja-conversion))
+  :config 
+   (selectrum-mode +1))
+
+
+(defun consult-jump-in-buffer ()
+  "Jump in buffer with `counsel-imenu' or `counsel-org-goto' if in org-mode"
+  (interactive)
+  (call-interactively
+   (cond
+    ((or (eq major-mode 'org-mode)
+         (member 'outline-mode minor-mode-list)) 
+     'consult-outline)
+    (t 'consult-imenu))))
+
+(defun consult-thing-at-point ()
+  "`consult' with `thing-at-point'."
+  (interactive)
+  (let ((thing (ivy-thing-at-point)))
+    (when (use-region-p)
+      (dectivate-mark))
+    (consult-line (regexp-quote thing))))
+
+(leaf selectrum-prescient
+  :ensure t
+  :config 
+  (selectrum-prescient-mode +1))
+
+
+(leaf ivy-prescient
+  :ensure t
+  :config 
+  (ivy-prescient-mode +1))
+
+(leaf company-prescient
+  :ensure t
+  :config 
+  (company-prescient-mode +1))
+
+(leaf embark
+  :ensure t
+  :bind (("S-<home>" . embark-act)))
